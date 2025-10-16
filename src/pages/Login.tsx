c98e59206efa27b28b44b02be4b30994a66f2db4
My Name <gunanivivek@gmail.com>
2025-10-16 12:34:55 +0530

    Signup navigation fixed

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -81,12 +81,12 @@ export default function Login() {
 
         <p className="text-base sm:text-lg text-gray-600 mt-4">
           New here?{" "}
-          <a
-            href="/signup"
+          <button
+            onClick={()=> navigate("/signup")}
             className="text-primary-300 hover:underline font-medium"
           >
             Sign up
-          </a>
+          </button>
         </p>
       </div>
     </div>
