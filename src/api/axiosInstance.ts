a4586d2584e7496b856b28d86c4f0d2d385f7331
My Name <gunanivivek@gmail.com>
2025-10-17 16:34:29 +0530

    Integrated the SignUp APIs with Frontend

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,17 +1,8 @@
 import axios from "axios";
-import { useAuthStore } from "../store/authStore";
-
+ 
 const API = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}`,
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true, // ✅ cookies enabled
 });
-
-// Attach token automatically
-API.interceptors.request.use((config) => {
-  const token = useAuthStore.getState().token;
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
-
+ 
 export default API;
