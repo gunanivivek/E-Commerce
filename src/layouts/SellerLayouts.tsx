2cd529887b5b4e5686ee5e4c364dc1dcf6cbcafb
My Name <gunanivivek@gmail.com>
2025-11-19 18:43:21 +0530

    Added skeleton and the notifications for newly placed orders

--- src/layouts/SellerLayouts.tsx
+++ src/layouts/SellerLayouts.tsx
@@ -1,7 +1,9 @@
 import { Outlet } from "react-router";
 import DashboardSidebar from "../components/DashboardSidebar";
+import { useNotificationSocket } from "../components/useNotificationSocket";
 
 const SellerLayouts = () => {
+  useNotificationSocket(); 
   return (
     <div className="flex flex-row min-h-screen">
       <DashboardSidebar />
